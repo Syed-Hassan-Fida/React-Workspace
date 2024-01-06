b8707e3ccef5420905a5cd28262b7168adf1017e
Hassan Fida <hassansfida@gmail.com>
2024-01-06 17:46:17 +0500

    useReducer counter

--- src/components/useReduer/MainUseReducer.jsx
+++ src/components/useReduer/MainUseReducer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CountOne from './CountOne'
 
 const MainUseReducer = () => {
   return (
     <div>
         <h1>MainUseReducer</h1>
+        <CountOne/>
     </div>
   )
 }
