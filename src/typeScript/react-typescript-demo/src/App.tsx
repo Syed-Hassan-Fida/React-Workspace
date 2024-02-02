c16746c99a07c42ca4d3c66f640d0131a5c83a5b
Hassan Fida <hassansfida@gmail.com>
2024-02-02 18:51:08 +0500

    type-script basics

--- src/typeScript/react-typescript-demo/src/App.tsx
+++ src/typeScript/react-typescript-demo/src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
 import Greet from './componnets/Greet';
+import Heading from './componnets/Heading';
 
 function App() {
+  const personObject = {
+    first: "ALi",
+    last: "Adil"
+  }
+
+  const arrayOfList = [
+    {
+      first: "ALi",
+      last: "Adil"
+    },
+    {
+      first: "ALi",
+      last: "Adil"
+    },
+    {
+      first: "ALi",
+      last: "Adil"
+    }
+  ]
   return (
     <div className="App">
-      <Greet name="ali"/>
+      <Greet name="ali" person={personObject} array={arrayOfList} status="loading" personCount={0}>
+        <Heading>PlaceHolder Text</Heading>
+      </Greet>
     </div>
   );
 }
--- src/typeScript/react-typescript-demo/src/componnets/Greet.tsx
+++ src/typeScript/react-typescript-demo/src/componnets/Greet.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React from "react"
+
 type GreetProps = {
-    name: string
+    name: string,
+    person: {
+        first: string,
+        last: string
+    },
+    array: {
+        first: string,
+        last: string
+    }[],
+    status: 'loading' | 'success' | 'error',
+    children?: React.ReactNode,
+    personCount?: number // '?' is for optional use in defining props 
 }
 const Greet = (props: GreetProps) => {
-  return (
-    <div>
-        <h1>Greet {props.name}</h1>
-    </div>
-  )
+    const {personCount = 0} = props 
+    let message
+    if (props.status === 'loading') {
+        message = 'laoding....'
+    } else if (props.status === 'success') {
+        message = 'success....'
+    } else if (props.status === 'error') {
+        message = 'error....'
+    } 
+    return (
+        <div>
+            <h1>Greet {props.name}</h1>
+            <h1>Status {message}</h1>
+            <h1>Person Count {personCount}</h1>
+            <h1>Hello {props.person.first} {props.person.last}</h1>
+            {
+                props.array.map(name => {
+                    return (
+                        <h2>{name.first}--{name.last}</h2>
+                    )
+                })
+            }
+            {props.children}
+        </div>
+    )
 }
 
 export default Greet
